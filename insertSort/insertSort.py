8fad9a3afe51330966a97bd49c6cc567f3bde56e
xuyongchuan <1216910603@qq.com>
2020-04-21 11:05:32 +0800

    插入排序

--- insertSort/insertSort.py
+++ insertSort/insertSort.py
@@ -4,4 +4,18 @@
 # @Author: xuyongchuan
 # @File  : insertSort.py.py
 
-def insertSort(nums):
+def insertionSort(nums):
+    for i in range(0, len(nums)-1):
+        current = nums[i]
+        while i-1 >= 0 and nums[i-1] > current:
+            nums[i] = nums[i-1]
+            i-=1
+        nums[i] = current
+    return nums
+
+
+if __name__ == '__main__':
+    test_nums = [7, 6, 5, 6, 3, 3, 9]
+    test_nums = insertionSort(test_nums)
+    print(test_nums)
+
