c31b2a24eb7689192cfecbb030f50d4ac9d7c93f
xuyongchuan <1216910603@qq.com>
2020-04-30 20:46:05 +0800

    add mergesort.py

---
+++ mergeSort/mergesort.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+# @Time  :2020/4/30 18:22
+# @Author: xuyongchuan
+# @File  : mergesort.py
+
+
+def mergesort(nums):
+    if len(nums)<2:
+        return nums
+    middle = len(nums)//2
+    left, right = nums[0:middle], nums[middle:]
+    return merge(mergesort(left), mergesort(right))
+
+
+def merge(left, right):
+    result = []
+    while left and right:
+        if left[0] <= right[0]:
+            result.append(left.pop(0))
+        else:
+            result.append(right.pop(0))
+    while left:
+        result.append(left.pop(0))
+    while right:
+        result.append(right.pop(0))
+    return result
+
+
+if __name__=='__main__':
+    test_nums = [7, 6, 5, 6, 3, 9, 3]
+    print(mergesort(test_nums))
