7e51eb443ecca05ebf0009caa903cb6bcfff1283
xuyongchuan <1216910603@qq.com>
2020-04-30 16:49:51 +0800

    fixed the following problem: the last element was not get sorted

--- insertSort/insertSort.py
+++ insertSort/insertSort.py
@@ -2,10 +2,11 @@
 # -*- coding:utf-8 -*-
 # @Time  :2020/4/20 22:40
 # @Author: xuyongchuan
-# @File  : insertSort.py.py
+# @File  : insertSort.py
 
-def insertionSort(nums):
-    for i in range(0, len(nums)-1):
+
+def insertionsort(nums):
+    for i in range(0, len(nums)):
         current = nums[i]
         while i-1 >= 0 and nums[i-1] > current:
             nums[i] = nums[i-1]
@@ -15,7 +16,7 @@ def insertionSort(nums):
 
 
 if __name__ == '__main__':
-    test_nums = [7, 6, 5, 6, 3, 3, 9]
-    test_nums = insertionSort(test_nums)
+    test_nums = [7, 6, 5, 6, 3, 9, 3]
+    test_nums = insertionsort(test_nums)
     print(test_nums)
 
